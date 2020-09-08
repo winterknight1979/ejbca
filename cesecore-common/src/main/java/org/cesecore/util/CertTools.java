7a174da5ca35fca17259addac50c2b5482e8381a
Dave Crisp <dave@goldeneyes.org.uk>
2020-09-08 22:49:21 +0100

    SecurityFilterInputStream

--- cesecore-common/src/main/java/org/cesecore/util/CertTools.java
+++ cesecore-common/src/main/java/org/cesecore/util/CertTools.java
@@ -18,111 +18,111 @@ import org.ejbca.cvc.exception.ParseException;
 
 public class CertTools {
 
-    private static final Logger log = Logger.getLogger(CertTools.class);
+	private static final Logger log = Logger.getLogger(CertTools.class);
 
-	  
-    /**
-     * Creates Certificate from byte[], can be either an X509 certificate or a CVCCertificate
-     * 
-     * @param cert byte array containing certificate in binary (DER) format, or PEM encoded X.509 certificate
-     * @param provider provider for example "SUN" or "BC", use null for the default provider (BC)
-     * @param returnType the type of Certificate to be returned. Certificate can be used if certificate type is unknown.
-     * 
-     * @return a Certificate 
-     * @throws CertificateParsingException if certificate couldn't be parsed from cert, or if the incorrect return type was specified.
-     * 
-     */
-	//@SuppressWarnings("unchecked")
-    public static <T extends Certificate> T getCertfromByteArray(byte[] cert, String provider, Class<T> returnType) throws CertificateParsingException {
-    	T ret = null;
-    	String prov = provider;
-    	if (prov == null) {
-    		prov = BouncyCastleProvider.PROVIDER_NAME;
-    	}
-    	
-    	if (returnType.equals(X509Certificate.class)) {
-    		ret = (T) parseX509Certificate(prov, cert);
-    	} else if (returnType.equals(CardVerifiableCertificate.class)) {
-            ret = (T) parseCardVerifiableCertificate(prov, cert);
-    	} else {
-    		// Let's guess
-    		try {
-    			 ret = (T) parseX509Certificate(prov, cert);
-    		} catch (CertificateParsingException e) {
-                try {
-                    ret = (T) parseCardVerifiableCertificate(prov, cert);
-                } catch (CertificateParsingException e1) {
-                    throw new CertificateParsingException("No certificate could be parsed from byte array. See debug logs for details.");
-                }
-            }
-    	}
-    	
-    	return ret;
+
+	/**
+	 * Creates Certificate from byte[], can be either an X509 certificate or a CVCCertificate
+	 * 
+	 * @param cert byte array containing certificate in binary (DER) format, or PEM encoded X.509 certificate
+	 * @param provider provider for example "SUN" or "BC", use null for the default provider (BC)
+	 * @param returnType the type of Certificate to be returned. Certificate can be used if certificate type is unknown.
+	 * 
+	 * @return a Certificate 
+	 * @throws CertificateParsingException if certificate couldn't be parsed from cert, or if the incorrect return type was specified.
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends Certificate> T getCertfromByteArray(byte[] cert, String provider, Class<T> returnType) throws CertificateParsingException {
+		T ret = null;
+		String prov = provider;
+		if (prov == null) {
+			prov = BouncyCastleProvider.PROVIDER_NAME;
+		}
+
+		if (returnType.equals(X509Certificate.class)) {
+			ret = (T) parseX509Certificate(prov, cert);
+		} else if (returnType.equals(CardVerifiableCertificate.class)) {
+			ret = (T) parseCardVerifiableCertificate(prov, cert);
+		} else {
+			// Let's guess
+			try {
+				ret = (T) parseX509Certificate(prov, cert);
+			} catch (CertificateParsingException e) {
+				try {
+					ret = (T) parseCardVerifiableCertificate(prov, cert);
+				} catch (CertificateParsingException e1) {
+					throw new CertificateParsingException("No certificate could be parsed from byte array. See debug logs for details.");
+				}
+			}
+		}
+
+		return ret;
 	}
-    
-    /**
-     * 
-     * @param provider a provider name 
-     * @param cert a byte array containing an encoded certificate
-     * @return a decoded X509Certificate
-     * @throws CertificateParsingException if the byte array wasn't valid, or contained a certificate other than an X509 Certificate. 
-     */
-    private static X509Certificate parseX509Certificate(String provider, byte[] cert) throws CertificateParsingException {
-    	final CertificateFactory cf = CertTools.getCertificateFactory(provider);
-    	X509Certificate result;
-    	try {  
-    		result = (X509Certificate) cf.generateCertificate(new SecurityFilterInputStream(new ByteArrayInputStream(cert))); 
-    	} catch (CertificateException e) {
-            throw new CertificateParsingException("Could not parse byte array as X509Certificate." + e.getCause().getMessage(), e);
-        }
-        if(result != null) {
-            return result;
-        } else {
-            throw new CertificateParsingException("Could not parse byte array as X509Certificate.");
-        }
-    }
-    
-  
+
+	/**
+	 * 
+	 * @param provider a provider name 
+	 * @param cert a byte array containing an encoded certificate
+	 * @return a decoded X509Certificate
+	 * @throws CertificateParsingException if the byte array wasn't valid, or contained a certificate other than an X509 Certificate. 
+	 */
+	private static X509Certificate parseX509Certificate(String provider, byte[] cert) throws CertificateParsingException {
+		final CertificateFactory cf = CertTools.getCertificateFactory(provider);
+		X509Certificate result;
+		try {  
+			result = (X509Certificate) cf.generateCertificate(new SecurityFilterInputStream(new ByteArrayInputStream(cert))); 
+		} catch (CertificateException e) {
+			throw new CertificateParsingException("Could not parse byte array as X509Certificate." + e.getCause().getMessage(), e);
+		}
+		if(result != null) {
+			return result;
+		} else {
+			throw new CertificateParsingException("Could not parse byte array as X509Certificate.");
+		}
+	}
+
+
 
 	private static CardVerifiableCertificate parseCardVerifiableCertificate(String provider, byte[] cert) throws CertificateParsingException {
-        // We could not create an X509Certificate, see if it is a CVC certificate instead
-        try {
-            final CVCertificate parsedObject = CertificateParser.parseCertificate(cert);
-            return new CardVerifiableCertificate(parsedObject);
-        } catch (ParseException e) {
-            throw new CertificateParsingException("ParseException trying to read CVCCertificate.", e);
-        } catch (ConstructionException e) {
-            throw new CertificateParsingException("ConstructionException trying to read CVCCertificate.", e);
-        } 
-    }
+		// We could not create an X509Certificate, see if it is a CVC certificate instead
+		try {
+			final CVCertificate parsedObject = CertificateParser.parseCertificate(cert);
+			return new CardVerifiableCertificate(parsedObject);
+		} catch (ParseException e) {
+			throw new CertificateParsingException("ParseException trying to read CVCCertificate.", e);
+		} catch (ConstructionException e) {
+			throw new CertificateParsingException("ConstructionException trying to read CVCCertificate.", e);
+		} 
+	}
 
-    /**
-     * @param returnType the type of Certificate to be returned, for example X509Certificate.class. Certificate.class can be used if certificate type is unknown.
-     * 
-     * @throws CertificateParsingException if the byte array does not contain a proper certificate.
-     */
+	/**
+	 * @param returnType the type of Certificate to be returned, for example X509Certificate.class. Certificate.class can be used if certificate type is unknown.
+	 * 
+	 * @throws CertificateParsingException if the byte array does not contain a proper certificate.
+	 */
 	public static <T extends Certificate> T getCertfromByteArray(byte[] cert, Class<T> returnType) throws CertificateParsingException {
 		return getCertfromByteArray(cert, BouncyCastleProvider.PROVIDER_NAME, returnType);
 	}
-	
-	  private static CertificateFactory getCertificateFactory( final String provider) {
-		  final String prov;
-	        if (provider == null) {
-	            prov = BouncyCastleProvider.PROVIDER_NAME;
-	        } else {
-	            prov = provider;
-	        }
-	        if (BouncyCastleProvider.PROVIDER_NAME.equals(prov)) {
-	            CryptoProviderTools.installBCProviderIfNotAvailable();
-	        }
-	        try {
-	            return CertificateFactory.getInstance("X.509", prov);
-	        } catch (NoSuchProviderException nspe) {
-	            log.error("NoSuchProvider: ", nspe);
-	        } catch (CertificateException ce) {
-	            log.error("CertificateException: ", ce);
-	        }
-	        return null;
+
+	private static CertificateFactory getCertificateFactory( final String provider) {
+		final String prov;
+		if (provider == null) {
+			prov = BouncyCastleProvider.PROVIDER_NAME;
+		} else {
+			prov = provider;
 		}
+		if (BouncyCastleProvider.PROVIDER_NAME.equals(prov)) {
+			CryptoProviderTools.installBCProviderIfNotAvailable();
+		}
+		try {
+			return CertificateFactory.getInstance("X.509", prov);
+		} catch (NoSuchProviderException nspe) {
+			log.error("NoSuchProvider: ", nspe);
+		} catch (CertificateException ce) {
+			log.error("CertificateException: ", ce);
+		}
+		return null;
+	}
 
 }
--- cesecore-common/src/main/java/org/cesecore/util/CryptoProviderTools.java
+++ cesecore-common/src/main/java/org/cesecore/util/CryptoProviderTools.java
@@ -5,16 +5,16 @@ import java.security.Security;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class CryptoProviderTools {
-	
-	 public static synchronized void installBCProviderIfNotAvailable() {
-	    	if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
-	    		installBCProvider();
-	    	}
-	    }
+
+	public static synchronized void installBCProviderIfNotAvailable() {
+		if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+			installBCProvider();
+		}
+	}
 
 	private static void installBCProvider() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
