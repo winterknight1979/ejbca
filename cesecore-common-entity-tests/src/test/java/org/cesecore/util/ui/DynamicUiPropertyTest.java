b366d8aa71fc082d181b6963073b7997bcb7885c
Dave Crisp <dave@goldeneyes.org.uk>
2020-09-12 22:23:47 +0100

    Cesecore-common tests that depend on cesecore-entity

---
+++ cesecore-common-entity-tests/src/test/java/org/cesecore/util/ui/DynamicUiPropertyTest.java
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.util.ui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import org.cesecore.roles.Role;
+import org.cesecore.roles.RoleData;
+import org.junit.Test;
+
+/**
+ * @version $Id: DynamicUiPropertyTest.java 34324 2020-01-17 12:22:39Z henriks $
+ *
+ */
+public class DynamicUiPropertyTest {
+
+    final static private String roleName = "anybody";
+    
+    @Test
+    public void testEncodingAndDecodingOfComplexType() throws PropertyValidationException {
+        RoleData anybody = new RoleData(new Role(null, roleName));
+        DynamicUiProperty<RoleData> roleProperty = new DynamicUiProperty<>("test",
+                anybody, new HashSet<>(Collections.singletonList(anybody)));
+        roleProperty.setHasMultipleValues(true);
+        List<String> encodedValues = roleProperty.getEncodedValues();  
+        DynamicUiProperty<RoleData> rolePropertyCopy = new DynamicUiProperty<>("test",
+                anybody, new HashSet<RoleData>());
+        rolePropertyCopy.setHasMultipleValues(true);
+        rolePropertyCopy.setEncodedValues(encodedValues);
+        assertTrue("RoleData object didn't survive encodement/decodement", rolePropertyCopy.getValues().contains(anybody));
+    }
+
+    @Test
+    public void testConstructors() {
+        final RoleData anybody = new RoleData(new Role(null, roleName));
+        DynamicUiProperty<RoleData> property = new DynamicUiProperty<>("someproperty", anybody);
+        checkPropertyState(property, "constructor with default value");
+
+        property = new DynamicUiProperty<>(property);
+        checkPropertyState(property, "copy constructor");
+    }
+
+    @Test
+    public void testSetValue() throws PropertyValidationException {
+        final RoleData anybody = new RoleData(new Role(null, roleName));
+        DynamicUiProperty<RoleData> property = new DynamicUiProperty<>();
+        property.setType(RoleData.class);
+        property.setValue(anybody);
+        checkPropertyState(property, "setValue(something)");
+        assertEquals(anybody, property.getValue());
+        property.setValue(null);
+        checkPropertyState(property, "setValue(null)");
+        property.setEncodedValue(new DynamicUiProperty<RoleData>().getAsEncodedValue(anybody));
+        checkPropertyState(property, "setEncodedValue(something)");
+        assertEquals(anybody, property.getValue());
+    }
+
+    private void checkPropertyState(final DynamicUiProperty<RoleData> property, final String step) {
+        property.getValue(); // Make sure we can get the value
+        try {
+            property.getValues();
+            fail("getValues() call should not be allowed (at step \"" + step + "\").");
+        } catch (IllegalStateException e) {
+            // NOPMD expected
+        }
+    }
+
+}
