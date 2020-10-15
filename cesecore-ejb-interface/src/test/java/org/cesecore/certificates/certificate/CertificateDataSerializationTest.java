53e7f8bcb444c695e61ecbb3fc5aea77c4d996d3
Dave Crisp <dave@goldeneyes.org.uk>
2020-10-15 13:12:28 +0100

    Cesecore-ejb-interface: fix failing test

--- cesecore-ejb-interface/src/test/java/org/cesecore/certificates/certificate/CertificateDataSerializationTest.java
+++ cesecore-ejb-interface/src/test/java/org/cesecore/certificates/certificate/CertificateDataSerializationTest.java
@@ -50,97 +50,59 @@ public class CertificateDataSerializationTest {
    * CertificateData object that's intentionally missing the endEntityProfileId
    * column.
    */
-  private static final String OLD_DATA =
-      "rO0ABXNyADVvcmcuY2VzZWNvcmUuY2VydGlma"
-      + "WNhdGVzLmNlcnRpZmljYXRlLkNlcnRpZmljYXRl"
-      + "RGF0YYoibbY+5VZeAgASSgAKZXhwaXJlRGF0ZUo"
-      + "ADnJldm9jYXRpb25EYXRlSQAQcmV2b2NhdGlv"
-      + "blJlYXNvbkkACnJvd1ZlcnNpb25JAAZzdGF0d"
-      + "XNJAAR0eXBlSgAKdXBkYXRlVGltZUwACmJhc2U2"
-      + "NENlcnR0ABJMamF2YS9sYW5nL1N0cmluZztMA"
-      + "A1jQUZpbmdlcnByaW50cQB+AAFMABRjZXJ0aWZp"
-      + "Y2F0ZVByb2ZpbGVJZHQAE0xqYXZhL2xhbmcvS"
-      + "W50ZWdlcjtMAAtmaW5nZXJwcmludHEAfgABTAAI"
-      + "aXNzdWVyRE5xAH4AAUwADXJvd1Byb3RlY3Rp"
-      + "b25xAH4AAUwADHNlcmlhbE51bWJlcnEAfgABTAAJ"
-      + "c3ViamVjdEROcQB+AAFMAAxzdWJqZWN0S2V5"
-      + "WRxAH4AAUwAA3RhZ3EAfgABTAAIdXNlcm5hbWVx"
-      + "AH4AAXhwAAABnfKkEeD///////////////8AA"
-      + "AAAAAAAFAAAAAEAAAFUhbhajXB0ABAxMjM0NTY3"
-      + "ODEyMzQ1Njc4c3IAEWphdmEubGFuZy5JbnRlZ"
-      + "2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZh"
-      + "LmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAA"
-      + "AF0ACgxOTRiNDcxMjJmMTcwMjA2NzcxNWVjZmIx"
-      + "MTYyMTE1N2M5NmNmMmFidAALQ049Y2VydHVzZ"
-      + "XJwdAATODc2NzYzODEyNDM1MTIyMjgzMXQAC0NO"
-      + "PWNlcnR1c2VydAAcY1U1UXF2NDR1T0JzVGpnQ"
-      + "Xh5WmNVYnZGNnNrPXB0AAhjZXJ0dXNlcg==";
+	private static final String OLD_DATA = 
+"rO0ABXNyADVvcmcuY2VzZWNvcmUuY2VydGlmaWNhdGVzLmNlcnRpZmljYXRlLkNlcnRpZmljYXRl"
++ "RGF0YYoibbY+5VZeAgASSgAKZXhwaXJlRGF0ZUoADnJldm9jYXRpb25EYXRlSQAQcmV2b2NhdGlv"
++ "blJlYXNvbkkACnJvd1ZlcnNpb25JAAZzdGF0dXNJAAR0eXBlSgAKdXBkYXRlVGltZUwACmJhc2U2"
++ "NENlcnR0ABJMamF2YS9sYW5nL1N0cmluZztMAA1jQUZpbmdlcnByaW50cQB+AAFMABRjZXJ0aWZp"
++ "Y2F0ZVByb2ZpbGVJZHQAE0xqYXZhL2xhbmcvSW50ZWdlcjtMAAtmaW5nZXJwcmludHEAfgABTAAI"
++ "aXNzdWVyRE5xAH4AAUwADXJvd1Byb3RlY3Rpb25xAH4AAUwADHNlcmlhbE51bWJlcnEAfgABTAAJ"
++ "c3ViamVjdEROcQB+AAFMAAxzdWJqZWN0S2V5SWRxAH4AAUwAA3RhZ3EAfgABTAAIdXNlcm5hbWVx"
++ "AH4AAXhwAAABnfKkEeD///////////////8AAAAAAAAAFAAAAAEAAAFUhbhajXB0ABAxMjM0NTY3"
++ "ODEyMzQ1Njc4c3IAEWphdmEubGFuZy5JbnRlZ2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZh"
++ "LmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAF0ACgxOTRiNDcxMjJmMTcwMjA2NzcxNWVjZmIx"
++ "MTYyMTE1N2M5NmNmMmFidAALQ049Y2VydHVzZXJwdAATODc2NzYzODEyNDM1MTIyMjgzMXQAC0NO"
++ "PWNlcnR1c2VydAAcY1U1UXF2NDR1T0JzVGpnQXh5WmNVYnZGNnNrPXB0AAhjZXJ0dXNlcg==";
 
   /**
    * CertificateData object that intentionally has a non-existing column called
    * "testFutureColumn".
    */
-  private static final String FUTURE_DATA =
-      "rO0ABXNyADVvcmcuY2VzZWNvcmUuY2VydGlma"
-      + "WNhdGVzLmNlcnRpZmljYXRlLkNlcnRpZmljYXRl"
-      + "RGF0YYoibbY+5VZeAgAUSgAKZXhwaXJlRGF0Z"
-      + "UoADnJldm9jYXRpb25EYXRlSQAQcmV2b2NhdGlv"
-      + "blJlYXNvbkkACnJvd1ZlcnNpb25JAAZzdGF0d"
-      + "XNJAAR0eXBlSgAKdXBkYXRlVGltZUwACmJhc2U2"
-      + "NENlcnR0ABJMamF2YS9sYW5nL1N0cmluZztMAA"
-      + "1jQUZpbmdlcnByaW50cQB+AAFMABRjZXJ0aWZp"
-      + "Y2F0ZVByb2ZpbGVJZHQAE0xqYXZhL2xhbmcvSW5"
-      + "0ZWdlcjtMABJlbmRFbnRpdHlQcm9maWxlSWRx"
-      + "AH4AAkwAC2ZpbmdlcnByaW50cQB+AAFMAAhpc3"
-      + "N1ZXJETnEAfgABTAANcm93UHJvdGVjdGlvbnEA"
-      + "fgABTAAMc2VyaWFsTnVtYmVycQB+AAFMAAlzdWJ"
-      + "qZWN0RE5xAH4AAUwADHN1YmplY3RLZXlJZHEA"
-      + "fgABTAADdGFncQB+AAFMABB0ZXN0RnV0dXJlQ29"
-      + "sdW1ucQB+AAJMAAh1c2VybmFtZXEAfgABeHAA"
-      + "AAGd8rEtWP///////////////wAAAAAAAAAUAAA"
-      + "AAQAAAVSFxXkJcHQAEDEyMzQ1Njc4MTIzNDU2"
-      + "NzhzcgARamF2YS5sYW5nLkludGVnZXIS4qCk94G"
-      + "HOAIAAUkABXZhbHVleHIAEGphdmEubGFuZy5O"
-      + "dW1iZXKGrJUdC5TgiwIAAHhwAAAAAXB0ACg0YT"
-      + "cyYTk0MGRhY2U3ZjcxYWE4OWYzYTcyNjVmYTRm"
-      + "MGUwYzFlMGY3dAALQ049Y2VydHVzZXJwdAATMzU"
-      + "wNzY2NTg4NjI3NTcyOTY3NXQAC0NOPWNlcnR1"
-      + "c2VydAAcUFg0eUc4cEs0b3JPZFBTU0hLMDMvOW"
-      + "tUcjBNPXBwdAAIY2VydHVzZXI=";
+	private static final String FUTURE_DATA =
+"rO0ABXNyADVvcmcuY2VzZWNvcmUuY2VydGlmaWNhdGVzLmNlcnRpZmljYXRlLkNlcnRpZmljYXRl"
++ "RGF0YYoibbY+5VZeAgAUSgAKZXhwaXJlRGF0ZUoADnJldm9jYXRpb25EYXRlSQAQcmV2b2NhdGlv"
++ "blJlYXNvbkkACnJvd1ZlcnNpb25JAAZzdGF0dXNJAAR0eXBlSgAKdXBkYXRlVGltZUwACmJhc2U2"
++ "NENlcnR0ABJMamF2YS9sYW5nL1N0cmluZztMAA1jQUZpbmdlcnByaW50cQB+AAFMABRjZXJ0aWZp"
++ "Y2F0ZVByb2ZpbGVJZHQAE0xqYXZhL2xhbmcvSW50ZWdlcjtMABJlbmRFbnRpdHlQcm9maWxlSWRx"
++ "AH4AAkwAC2ZpbmdlcnByaW50cQB+AAFMAAhpc3N1ZXJETnEAfgABTAANcm93UHJvdGVjdGlvbnEA"
++ "fgABTAAMc2VyaWFsTnVtYmVycQB+AAFMAAlzdWJqZWN0RE5xAH4AAUwADHN1YmplY3RLZXlJZHEA"
++ "fgABTAADdGFncQB+AAFMABB0ZXN0RnV0dXJlQ29sdW1ucQB+AAJMAAh1c2VybmFtZXEAfgABeHAA"
++ "AAGd8rEtWP///////////////wAAAAAAAAAUAAAAAQAAAVSFxXkJcHQAEDEyMzQ1Njc4MTIzNDU2"
++ "NzhzcgARamF2YS5sYW5nLkludGVnZXIS4qCk94GHOAIAAUkABXZhbHVleHIAEGphdmEubGFuZy5O"
++ "dW1iZXKGrJUdC5TgiwIAAHhwAAAAAXB0ACg0YTcyYTk0MGRhY2U3ZjcxYWE4OWYzYTcyNjVmYTRm"
++ "MGUwYzFlMGY3dAALQ049Y2VydHVzZXJwdAATMzUwNzY2NTg4NjI3NTcyOTY3NXQAC0NOPWNlcnR1"
++ "c2VydAAcUFg0eUc4cEs0b3JPZFBTU0hLMDMvOWtUcjBNPXBwdAAIY2VydHVzZXI=";
 
   /**
    * CertificateData object that has a non-existent field "testNonExistentClass"
    * with of a non-existent type.
    * "org.cesecore.certificates.certificate.NonExistent"
    */
-  private static final String FUTURE_NEW_CLASS_DATA =
-      "rO0ABXNyADVvcmcuY2VzZWNvcmUuY2VydGlmaWNhd"
-      + "GVzLmNlcnRpZmljYXRlLkNlcnRpZmljYXRl"
-      + "RGF0YYoibbY+5VZeAgAUSgAKZXhwaXJlRGF0Z"
-      + "UoADnJldm9jYXRpb25EYXRlSQAQcmV2b2NhdGlv"
-      + "blJlYXNvbkkACnJvd1ZlcnNpb25JAAZzdGF0dX"
-      + "NJAAR0eXBlSgAKdXBkYXRlVGltZUwACmJhc2U2"
-      + "NENlcnR0ABJMamF2YS9sYW5nL1N0cmluZztMAA"
-      + "1jQUZpbmdlcnByaW50cQB+AAFMABRjZXJ0aWZp"
-      + "Y2F0ZVByb2ZpbGVJZHQAE0xqYXZhL2xhbmcvSW5"
-      + "0ZWdlcjtMABJlbmRFbnRpdHlQcm9maWxlSWRx"
-      + "AH4AAkwAC2ZpbmdlcnByaW50cQB+AAFMAAhpc3"
-      + "N1ZXJETnEAfgABTAANcm93UHJvdGVjdGlvbnEA"
-      + "fgABTAAMc2VyaWFsTnVtYmVycQB+AAFMAAlzdWJ"
-      + "qZWN0RE5xAH4AAUwADHN1YmplY3RLZXlJZHEA"
-      + "fgABTAADdGFncQB+AAFMABR0ZXN0Tm9uRXhpc3R"
-      + "lbnRDbGFzc3QAI0xvcmcvY2VzZWNvcmUvaW50"
-      + "ZXJuYWwvTm9uRXhpc3RlbnQ7TAAIdXNlcm5hbWVx"
-      + "AH4AAXhwAAABnk5S8Nj///////////////8A"
-      + "AAAAAAAAFAAAAAEAAAFU4Wc5Z3B0ABAxMjM0NTY"
-      + "3ODEyMzQ1Njc4c3IAEWphdmEubGFuZy5JbnRl"
-      + "Z2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZhL"
-      + "mxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAA"
-      + "AAFwdAAoNjlkOWEzYzI3N2ViYjg4ODZmMzQzYTRi"
-      + "NWEzM2ViYTVlNDJlZmEyM3QAC0NOPWNlcnR1"
-      + "c2VycHQAEzYyODQxMDQwODE0Mjg3NzQxMTJ0AAtD"
-      + "Tj1jZXJ0dXNlcnQAHFdXaUJwdTZidFVTRzZZ"
-      + "RE4yT1dnazJsYi95ND1wcHQACGNlcnR1c2Vy";
+	private static final String FUTURE_NEW_CLASS_DATA =
+"rO0ABXNyADVvcmcuY2VzZWNvcmUuY2VydGlmaWNhdGVzLmNlcnRpZmljYXRlLkNlcnRpZmljYXRl"
++ "RGF0YYoibbY+5VZeAgAUSgAKZXhwaXJlRGF0ZUoADnJldm9jYXRpb25EYXRlSQAQcmV2b2NhdGlv"
++ "blJlYXNvbkkACnJvd1ZlcnNpb25JAAZzdGF0dXNJAAR0eXBlSgAKdXBkYXRlVGltZUwACmJhc2U2"
++ "NENlcnR0ABJMamF2YS9sYW5nL1N0cmluZztMAA1jQUZpbmdlcnByaW50cQB+AAFMABRjZXJ0aWZp"
++ "Y2F0ZVByb2ZpbGVJZHQAE0xqYXZhL2xhbmcvSW50ZWdlcjtMABJlbmRFbnRpdHlQcm9maWxlSWRx"
++ "AH4AAkwAC2ZpbmdlcnByaW50cQB+AAFMAAhpc3N1ZXJETnEAfgABTAANcm93UHJvdGVjdGlvbnEA"
++ "fgABTAAMc2VyaWFsTnVtYmVycQB+AAFMAAlzdWJqZWN0RE5xAH4AAUwADHN1YmplY3RLZXlJZHEA"
++ "fgABTAADdGFncQB+AAFMABR0ZXN0Tm9uRXhpc3RlbnRDbGFzc3QAI0xvcmcvY2VzZWNvcmUvaW50"
++ "ZXJuYWwvTm9uRXhpc3RlbnQ7TAAIdXNlcm5hbWVxAH4AAXhwAAABnk5S8Nj///////////////8A"
++ "AAAAAAAAFAAAAAEAAAFU4Wc5Z3B0ABAxMjM0NTY3ODEyMzQ1Njc4c3IAEWphdmEubGFuZy5JbnRl"
++ "Z2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAA"
++ "AAFwdAAoNjlkOWEzYzI3N2ViYjg4ODZmMzQzYTRiNWEzM2ViYTVlNDJlZmEyM3QAC0NOPWNlcnR1"
++ "c2VycHQAEzYyODQxMDQwODE0Mjg3NzQxMTJ0AAtDTj1jZXJ0dXNlcnQAHFdXaUJwdTZidFVTRzZZ"
++ "RE4yT1dnazJsYi95ND1wcHQACGNlcnR1c2Vy";
 /** Setup. */
   @BeforeClass
   public static void beforeClass() {
