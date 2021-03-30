61540e12d2d3ce964a28a7dc96fa49606099d50b
Dave Crisp <david.crisp@ast-uk.com>
2021-03-30 14:37:51 +0100

    cmp-tcp: checkstyle all

--- ejbca-cmp-tcp/src/main/java/org/ejbca/ui/tcp/TcpReceivedMessage.java
+++ ejbca-cmp-tcp/src/main/java/org/ejbca/ui/tcp/TcpReceivedMessage.java
@@ -16,95 +16,120 @@ package org.ejbca.ui.tcp;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
-
 import org.apache.log4j.Logger;
 import org.cesecore.util.Base64;
 import org.ejbca.core.model.InternalEjbcaResources;
 
 /**
  * Decodes a TCP messages from a client.
- * 
+ *
  * @author lars
  * @version $Id: TcpReceivedMessage.java 19902 2014-09-30 14:32:24Z anatom $
- *
  */
-public class TcpReceivedMessage {
-	private static final Logger log = Logger.getLogger(TcpReceivedMessage.class.getName());
-	/** Internal localization of logs and errors */
-	private static final InternalEjbcaResources intres = InternalEjbcaResources.getInstance();
-	/**
-	 * true if the session should be closed after returning to the client
-	 */
-	public final boolean doClose;
-	/**
-	 * the message from the client decoded from ASN1
-	 */
-	public final byte[] message;
+public final class TcpReceivedMessage {
+    /** Logger. */
+  private static final Logger LOG =
+      Logger.getLogger(TcpReceivedMessage.class.getName());
+  /** Internal localization of logs and errors. */
+  private static final InternalEjbcaResources INTRES =
+      InternalEjbcaResources.getInstance();
+  /** true if the session should be closed after returning to the client. */
+  private final boolean doClose;
+  /** the message from the client decoded from ASN1. */
+  private final byte[] message;
 
-	private TcpReceivedMessage() { // this notifies an error
-		this.doClose = true;
-		this.message = null;
-	}
+  private TcpReceivedMessage() { // this notifies an error
+    this.doClose = true;
+    this.message = null;
+  }
 
-	private TcpReceivedMessage( boolean close, byte[] message) { // message OK
-		this.doClose = close;
-		this.message = message;
-	}
-	/**
-	 * @param command bytes from client. The payload of has to be ASN1 encoded
-	 * @return the message ASN1 decoded
-	 * @throws IOException Fail
-	 */
-	static public TcpReceivedMessage getTcpMessage(byte command[]) throws IOException {
-		if ( command==null || command.length==0 ) {
-			return new TcpReceivedMessage(); // this is something fishy
-		}
-		if (log.isTraceEnabled()) {
-			log.trace("Got data of length "+command.length+": "+new String(Base64.encode(command)));			
-		}
-		final int cmpMessageStartOffset = 7;
-		if (command.length <= cmpMessageStartOffset) {
-			return new TcpReceivedMessage();
-		}
-		final ByteArrayInputStream bai = new ByteArrayInputStream(command);
-		final DataInputStream dis = new DataInputStream(bai);
-		// Read the length, 32 bits
-		final int len = dis.readInt(); // 4 bytes
-		log.debug("Got a message claiming to be of length: " + len);
+  private TcpReceivedMessage(
+      final boolean close, final byte[] amessage) { // message OK
+    this.doClose = close;
+    this.message = amessage;
+  }
+  /**
+   * @param command bytes from client. The payload of has to be ASN1 encoded
+   * @return the message ASN1 decoded
+   * @throws IOException Fail
+   */
+  public static TcpReceivedMessage getTcpMessage(final byte[] command)
+      throws IOException {
+    if (command == null || command.length == 0) {
+      return new TcpReceivedMessage(); // this is something fishy
+    }
+    if (LOG.isTraceEnabled()) {
+      LOG.trace(
+          "Got data of length "
+              + command.length
+              + ": "
+              + new String(Base64.encode(command)));
+    }
+    final int cmpMessageStartOffset = 7;
+    if (command.length <= cmpMessageStartOffset) {
+      return new TcpReceivedMessage();
+    }
+    final ByteArrayInputStream bai = new ByteArrayInputStream(command);
+    final DataInputStream dis = new DataInputStream(bai);
+    // Read the length, 32 bits
+    final int len = dis.readInt(); // 4 bytes
+    LOG.debug("Got a message claiming to be of length: " + len);
 
-		// Read the version, 8 bits. Version should be 10 (protocol draft nr 5)
-		final int ver = dis.readByte(); // 1 byte
-		log.debug("Got a message with version: " + ver);
+    // Read the version, 8 bits. Version should be 10 (protocol draft nr 5)
+    final int ver = dis.readByte(); // 1 byte
+    LOG.debug("Got a message with version: " + ver);
 
-		// Read flags, 8 bits for version 10
-		final byte flags = dis.readByte(); // 1 byte
-		log.debug("Got a message with flags (1 means close): " + flags);
-		// 'flags' will be used to check if the client wants us to close the connection (LSB is 1 in that case according to spec)
+    // Read flags, 8 bits for version 10
+    final byte flags = dis.readByte(); // 1 byte
+    LOG.debug("Got a message with flags (1 means close): " + flags);
+    // 'flags' will be used to check if the client wants us to close the
+    // connection (LSB is 1 in that case according to spec)
 
-		// Read message type, 8 bits
-		final int msgType = dis.readByte(); // 1 byte
-		// now 'payLoadStrartOffset' bytes has been read
-		log.debug("Got a message of type: " +msgType);
+    // Read message type, 8 bits
+    final int msgType = dis.readByte(); // 1 byte
+    // now 'payLoadStrartOffset' bytes has been read
+    LOG.debug("Got a message of type: " + msgType);
 
-		// Read message
-		final int msgLen = command.length - 4;
-		// They should match
-		if ( len!=msgLen ) {
-			log.error( intres.getLocalizedMessage("cmp.errortcpwronglen", Integer.valueOf(msgLen), Integer.valueOf(len)) );
-			return new TcpReceivedMessage();// This is something malicious
-		}
-		if ( msgLen>=5000 ) {
-			log.error( intres.getLocalizedMessage("cmp.errortcptoolongmsg", Integer.valueOf(msgLen)) );
-			return new TcpReceivedMessage();// This is something malicious
-		}
-		// The CMP message is the rest of the stream that has not been read yet.
-		try {
-			byte[] ba = new byte[command.length-cmpMessageStartOffset];
-			dis.read(ba);
-			return new TcpReceivedMessage( (flags&0x01)>0, ba);
-		} catch( Throwable e ) { // NOPMD: any error return empty
-			log.error( intres.getLocalizedMessage("cmp.errornoasn1"), e );
-			return new TcpReceivedMessage();
-		}
-	}
+    // Read message
+    final int msgLen = command.length - 4;
+    // They should match
+    if (len != msgLen) {
+      LOG.error(
+          INTRES.getLocalizedMessage(
+              "cmp.errortcpwronglen",
+              Integer.valueOf(msgLen),
+              Integer.valueOf(len)));
+      return new TcpReceivedMessage(); // This is something malicious
+    }
+    final int max = 5000;
+    if (msgLen >= max) {
+      LOG.error(
+          INTRES.getLocalizedMessage(
+              "cmp.errortcptoolongmsg", Integer.valueOf(msgLen)));
+      return new TcpReceivedMessage(); // This is something malicious
+    }
+    // The CMP message is the rest of the stream that has not been read yet.
+    try {
+      byte[] ba = new byte[command.length - cmpMessageStartOffset];
+      dis.read(ba);
+      return new TcpReceivedMessage((flags & 0x01) > 0, ba);
+    } catch (Throwable e) { // NOPMD: any error return empty
+      LOG.error(INTRES.getLocalizedMessage("cmp.errornoasn1"), e);
+      return new TcpReceivedMessage();
+    }
+  }
+
+/**
+ * @return the doClose
+ */
+public boolean isDoClose() {
+    return doClose;
+}
+
+/**
+ * @return the message
+ */
+public byte[] getMessage() {
+    return message;
+}
 }
--- ejbca-cmp-tcp/src/main/java/org/ejbca/ui/web/protocol/CmpTcpInitServlet.java
+++ ejbca-cmp-tcp/src/main/java/org/ejbca/ui/web/protocol/CmpTcpInitServlet.java
@@ -14,44 +14,46 @@
 package org.ejbca.ui.web.protocol;
 
 import java.net.UnknownHostException;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-
 import org.apache.log4j.Logger;
 import org.ejbca.ui.tcp.CmpTcpServer;
 
 /**
- * Servlet with the sole purpose of starting the CMP TCP server. 
+ * Servlet with the sole purpose of starting the CMP TCP server.
  *
  * @version $Id: CmpTcpInitServlet.java 19902 2014-09-30 14:32:24Z anatom $
  */
 public class CmpTcpInitServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	private static final Logger LOG = Logger.getLogger(CmpTcpInitServlet.class);
-	
-	private transient CmpTcpServer cmpTcpServer;	// HttpServlet implements Serializable
+  private static final long serialVersionUID = 1L;
+  /** Logger. */
+  private static final Logger LOG = Logger.getLogger(CmpTcpInitServlet.class);
+/** Param. */
+  private transient CmpTcpServer
+      cmpTcpServer; // HttpServlet implements Serializable
 
-	public void init(final ServletConfig config) throws ServletException {
-		super.init(config);
-		LOG.info("Starting CMP TCP Service..");
-		cmpTcpServer = new CmpTcpServer();
-		try {
-			cmpTcpServer.start();
-		} catch (UnknownHostException e) {
-			throw new ServletException(e);
-		}
-	}
-	
-	public void destroy() {
-		LOG.info("Stopping CMP TCP Service..");
-		try {
-			cmpTcpServer.stop();
-		} catch (Throwable t) { // NOPMD: never bail out and crash here
-			LOG.error("", t);
-		}
-		super.destroy();
-	}
+  @Override
+  public void init(final ServletConfig config) throws ServletException {
+    super.init(config);
+    LOG.info("Starting CMP TCP Service..");
+    cmpTcpServer = new CmpTcpServer();
+    try {
+      cmpTcpServer.start();
+    } catch (UnknownHostException e) {
+      throw new ServletException(e);
+    }
+  }
+
+  @Override
+  public void destroy() {
+    LOG.info("Stopping CMP TCP Service..");
+    try {
+      cmpTcpServer.stop();
+    } catch (Throwable t) { // NOPMD: never bail out and crash here
+      LOG.error("", t);
+    }
+    super.destroy();
+  }
 }
