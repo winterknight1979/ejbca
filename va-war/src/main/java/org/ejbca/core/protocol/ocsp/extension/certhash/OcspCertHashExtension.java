0445b872f1f799a805dd57551895470c8758547c
Dave Crisp <david.crisp@ast-uk.com>
2021-03-30 14:53:59 +0100

    va-war: complete checkstyle

--- va-war/src/main/java/org/ejbca/core/protocol/ocsp/extension/certhash/OcspCertHashExtension.java
+++ va-war/src/main/java/org/ejbca/core/protocol/ocsp/extension/certhash/OcspCertHashExtension.java
@@ -21,7 +21,6 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.DEROctetString;
@@ -33,81 +32,104 @@ import org.cesecore.certificates.ocsp.extension.OCSPExtensionType;
 import org.cesecore.keybind.InternalKeyBinding;
 
 /**
- * ASN.1 OCSP extension used to return hash values for certificates as part of an OCSP response.
- * 
- * Certificates will be encoded with SHA256
- * 
- * OID for this extension is 1.3.36.8.3.13
- * 
- * This extension is specified in the German Common PKI standard.
- * <a href="http://www.t7ev.org/T7-de/Common-PKI">Common PKI</a> SigG CertHash OCSP extension.
- * 
- * CertHashExtension is a SINGLE_RESPONSE extension, which means it's inside the responseItem, 
- * not generic for the Response (which can contain several responseItems)
- * 
- * @version $Id: OcspCertHashExtension.java 28881 2018-05-08 18:18:11Z anatom $
+ * ASN.1 OCSP extension used to return hash values for certificates as part of
+ * an OCSP response.
  *
+ * <p>Certificates will be encoded with SHA256
+ *
+ * <p>OID for this extension is 1.3.36.8.3.13
+ *
+ * <p>This extension is specified in the German Common PKI standard. <a
+ * href="http://www.t7ev.org/T7-de/Common-PKI">Common PKI</a> SigG CertHash OCSP
+ * extension.
+ *
+ * <p>CertHashExtension is a SINGLE_RESPONSE extension, which means it's inside
+ * the responseItem, not generic for the Response (which can contain several
+ * responseItems)
+ *
+ * @version $Id: OcspCertHashExtension.java 28881 2018-05-08 18:18:11Z anatom $
  */
 public class OcspCertHashExtension implements OCSPExtension {
 
-    public static final String CERT_HASH_OID = "1.3.36.8.3.13";
-    public static final String CERT_HASH_NAME = "Certificate Hash";
-    public static final ASN1ObjectIdentifier SHA256 = new ASN1ObjectIdentifier("2.16.840.1.101.3.4.2.1");
+      /** Param. */
+  public static final String CERT_HASH_OID = "1.3.36.8.3.13";
+  /** Param. */
+  public static final String CERT_HASH_NAME = "Certificate Hash";
+  /** Param. */
+  public static final ASN1ObjectIdentifier SHA256 =
+      new ASN1ObjectIdentifier("2.16.840.1.101.3.4.2.1");
 
-    private static final Logger log = Logger.getLogger(OcspCertHashExtension.class);
-    
-    @Override
-    public String getOid() {
-        return CERT_HASH_OID;
-    }
-    
-    @Override
-    public String getName() {
-        return CERT_HASH_NAME;
-    }
-    
-    @Override
-    public Set<OCSPExtensionType> getExtensionType() {
-        return EnumSet.of(OCSPExtensionType.SINGLE_RESPONSE);
-    }
-    
-    @Override
-    public void init() {
-       //Nothing much to do here.      
-    }
+  /** Param. */
+  private static final Logger LOG =
+      Logger.getLogger(OcspCertHashExtension.class);
 
-    @Override
-    public Map<ASN1ObjectIdentifier, Extension> process(X509Certificate[] requestCertificates, String remoteAddress, String remoteHost,
-            X509Certificate cert, org.bouncycastle.cert.ocsp.CertificateStatus status, InternalKeyBinding internalKeyBinding) {
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA256");
-        } catch (NoSuchAlgorithmException e) {
-            //This state can't be handled, shouldn't return null 
-            log.error("Could not create MessageDigest with algorithm SHA256", e);
-            throw new IllegalStateException("Could not create MessageDigest with algorithm SHA256", e);
-        }
-        CertHash certHash;
-        try {
-            certHash = new CertHash(new AlgorithmIdentifier(SHA256), md.digest(cert.getEncoded()));
-        } catch (CertificateEncodingException e) {
-            //This state can't be handled, shouldn't return null 
-            log.error("Could not encode certificate " + cert, e);
-            throw new IllegalStateException("Could not encode certificate " + cert, e);
-        }
-        HashMap<ASN1ObjectIdentifier, Extension> result = new HashMap<ASN1ObjectIdentifier, Extension>();
-        try {
-            result.put(new ASN1ObjectIdentifier(CERT_HASH_OID), new Extension(new ASN1ObjectIdentifier(CERT_HASH_OID), false,
-                    new DEROctetString(certHash)));
-        } catch (IOException e) {
-            throw new IllegalStateException("Could not construct an ASN.1Primitive.", e);
-        }
-        return result;
-    }
+  @Override
+  public String getOid() {
+    return CERT_HASH_OID;
+  }
 
-    @Override
-    public int getLastErrorCode() {
-        //No error codes defined
-        return 0;
+  @Override
+  public String getName() {
+    return CERT_HASH_NAME;
+  }
+
+  @Override
+  public Set<OCSPExtensionType> getExtensionType() {
+    return EnumSet.of(OCSPExtensionType.SINGLE_RESPONSE);
+  }
+
+  @Override
+  public void init() {
+    // Nothing much to do here.
+  }
+
+  @Override
+  public Map<ASN1ObjectIdentifier, Extension> process(
+      final X509Certificate[] requestCertificates,
+      final String remoteAddress,
+      final String remoteHost,
+      final X509Certificate cert,
+      final org.bouncycastle.cert.ocsp.CertificateStatus status,
+      final InternalKeyBinding internalKeyBinding) {
+    MessageDigest md = null;
+    try {
+      md = MessageDigest.getInstance("SHA256");
+    } catch (NoSuchAlgorithmException e) {
+      // This state can't be handled, shouldn't return null
+      LOG.error("Could not create MessageDigest with algorithm SHA256", e);
+      throw new IllegalStateException(
+          "Could not create MessageDigest with algorithm SHA256", e);
     }
+    CertHash certHash;
+    try {
+      certHash =
+          new CertHash(
+              new AlgorithmIdentifier(SHA256), md.digest(cert.getEncoded()));
+    } catch (CertificateEncodingException e) {
+      // This state can't be handled, shouldn't return null
+      LOG.error("Could not encode certificate " + cert, e);
+      throw new IllegalStateException(
+          "Could not encode certificate " + cert, e);
+    }
+    HashMap<ASN1ObjectIdentifier, Extension> result =
+        new HashMap<ASN1ObjectIdentifier, Extension>();
+    try {
+      result.put(
+          new ASN1ObjectIdentifier(CERT_HASH_OID),
+          new Extension(
+              new ASN1ObjectIdentifier(CERT_HASH_OID),
+              false,
+              new DEROctetString(certHash)));
+    } catch (IOException e) {
+      throw new IllegalStateException(
+          "Could not construct an ASN.1Primitive.", e);
+    }
+    return result;
+  }
+
+  @Override
+  public int getLastErrorCode() {
+    // No error codes defined
+    return 0;
+  }
 }
